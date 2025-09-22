3d1be14d0d0f261fb4f2238b75d5c3f0a22aabc2
zhuo_xuan <zhuo_xuan@otronic.cn>
2025-09-22 17:40:31 +0800

    add:添加qss

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include "mainwindow.h"
 #include <QApplication>
+#include <QFile>
 
 int main(int argc, char* argv[]) {
     QApplication a(argc, argv);
+    QFile file(":/style/style.qss");
     mainwindow w;
     w.show();
     return a.exec();
